b85d223c4ff80e56cef10953b8f32dec7137a19e
FI86 <105541108+FI86@users.noreply.github.com>
2026-06-15 14:09:03 +0200

    Add files via upload

--- Corriges/Cours/web/03_urlliberr.py
+++ Corriges/Cours/web/03_urlliberr.py
@@ -16,13 +16,18 @@ def main():
     # Génère URLError
     url = "http://no-such-server.org"
     # Génère HTTPError
-    # url = "http://httpbin.org/status/404" if UTILISE_HTTPBIN else "https://postman-echo.com/status/404"
+    url = "http://httpbin.org/status/404" if UTILISE_HTTPBIN else "https://www.python.org/does-not-exist"
     # Devrait fonctionner
-    # url = "http://httpbin.org/html" if UTILISE_HTTPBIN else "https://postman-echo.com/get"
+    url = "http://httpbin.org/html" if UTILISE_HTTPBIN else "https://www.iana.org/assignments/character-sets/character-sets.xhtml?utm_source=chatgpt.com"
+
+
+    # Creation d'une requete avec un header pour simuler un navigateur et eviter erreur 403 par les sites.
+    # pas obligatoire, mais sans ca beaucoup de site bloque la demande d'acces.
+    req = urllib.request.Request(url, headers={"User-Agent": "Mozilla/5.0"})
 
     # Utiliser la gestion des exceptions pour tenter l'accès à l'URL
     try:
-        resultat: HTTPResponse = urllib.request.urlopen(url)
+        resultat: HTTPResponse = urllib.request.urlopen(req)
         print(f"Code retour : {resultat.status}")
 
         if (resultat.getcode() == HTTPStatus.OK):
@@ -30,6 +35,7 @@ def main():
     # Se produit lorsque le serveur renvoie un code d'erreur de non-succès
     except HTTPError as err:
         print(f"Erreur : {err.code}")
+        print(f"URL : {err.url}")
     # Se produit lorsque quelque chose ne va pas avec l'URL elle-même
     except URLError as err:
         print(f"Ce serveur n'existe pas. {err.reason}")
